de926dd718dcf4c2f29c141c01e149b311d04c79
Aza444 <125324304+Aza444@users.noreply.github.com>
2023-08-10 19:05:41 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// Declaración de variables y luego inicializarlas a cero.
+using System;
+
+float num1 = 0; int num2 = 0;
+// Mostrar título como la aplicación de calculadora de consola C#.
+Console.WriteLine("Calculadora en modo consola en C#\r");
+Console.WriteLine("---------------------------------------\n");
+// Pedir al usuario que escriba el primer número.
+Console.WriteLine("Escriba un número y luego presione Entrar");
+num1 = Convert.ToInt32(Console.ReadLine());
+// Pedir al usuario que escriba el segundo número.
+Console.WriteLine("Escriba el otro número y luego presione Entrar");
+num2 = Convert.ToInt32(Console.ReadLine());
+// Pedir al usuario que elija una opción..
+Console.WriteLine("Elija una opción de la siguiente lista:");
+Console.WriteLine("\ts - Suma");
+Console.WriteLine("\tr - Resta");
+Console.WriteLine("\tm - Multiplicacion");
+Console.WriteLine("\td - Division");
+Console.WriteLine("\t¿Qué opción desea realizar?");
+// Use una declaración de switch para hacer los cálculos.
+switch (Console.ReadLine())
+{
+    case "s":
+        Console.WriteLine($"El resultado de tu operación es: {num1} + {num2} = " + (num1 +
+        num2));
+        break;
+    case "r":
+        Console.WriteLine($"El resultado de tu operación es: {num1} - {num2} = " + (num1 -
+        num2));
+        break;
+    case "m":
+        Console.WriteLine($"El resultado de tu operación es: {num1} * {num2} = " + (num1 *
+        num2));
+        break;
+    case "d":
+        // Ask the user to enter a non-zero divisor until they do so.
+while (num2 == 0)
+        {
+            Console.WriteLine("Introduzca un divisor distinto de cero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine($"El resultado de tu operación es: {num1} / {num2} = " + (num1 /
+        num2));
+        break;
+}
+// Espere a que el usuario responda antes de cerrar.
+Console.Write("Presione cualquier tecla para cerrar la aplicación de la consola\nCalculadora...");
+Console.ReadKey();
